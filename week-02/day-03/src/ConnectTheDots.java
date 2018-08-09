2093285fc1d07399b95420abceb4836db97023b8
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-09 22:41:18 +0200

    ConnectTheDots finished -works

--- week-02/day-03/src/ConnectTheDots.java
+++ week-02/day-03/src/ConnectTheDots.java
@@ -7,23 +7,23 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class ConnectTheDots {
   public static void mainDraw(Graphics graphics) {
-
-
+    graphics.setColor(Color.GREEN);
+    int[][] coordinates1 = new int[][]{{10, 10}, {290, 10}, {290, 290}, {10, 290}};
+    int[][] coordinates2 = new int[][]{{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70}, {120, 100}, {85, 130}, {50, 100}};
+    coordinates(graphics, coordinates1);
+    coordinates(graphics, coordinates2);
   }
 
-  public static void coordinates(Graphics graphics, int[][] array) {
-    int x = 0;
-    int y = 0;
-    array = new int[][]{{x, x}, {y, y}};
-    graphics.drawLine(array[x][0], array[x][1], array[y][0], array[y][1]);
-
+  public static Graphics coordinates(Graphics graphics, int[][] array) {
+    for (int i = 0; i <= array.length - 1; i++) {
+      if (i != array.length - 1)
+        graphics.drawLine(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      else {
+        graphics.drawLine(array[i][0], array[i][1], array[i - array.length + 1][0], array[i - array.length + 1][1]);
+      }
+    }
+    return graphics;
   }
-  // Create a function that takes 1 parameter:
-  // An array of {x, y} points
-  // and connects them with green lines.
-  // Connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}}
-  // Connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
-  // {120, 100}, {85, 130}, {50, 100}}
 
   // Don't touch the code below
   static int WIDTH = 300;
