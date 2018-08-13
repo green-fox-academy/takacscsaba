2c64403719944559b19549effe03649e0431f377
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-13 14:16:40 +0200

    GuessMyNumber finished

--- week-02/day-01/src/GuessMyNumber.java
+++ week-02/day-01/src/GuessMyNumber.java
@@ -3,29 +3,29 @@ import java.util.Scanner;
 public class GuessMyNumber {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Please enter the maximum number: ");
+    System.out.print("Please enter the maximum number: ");
     int input = scanner.nextInt();
-
-
+    myNum(input);
   }
-  public static String myNum(String string, int input) {
-    double exactNumber = Math.random() * input;
+
+  public static void myNum(int input) {
+    int exactNumber = 1 + (int) (Math.random() * input);
     int lives = 5;
 
-    for (int i = 0; i < 5; i++) {
-      //scanner, input, lives
+    for (int i = 5; i >= 0; i--) {
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Please enter your guess between 1 and " + input + ": ");
+      int guess = scanner.nextInt();
+      if (i == 0) {
+        System.out.println("You died. The number was: " + exactNumber);
+      } else if (guess < exactNumber) {
+        System.out.println("Too low. You have only " + i + " lives left.");
+      } else if (guess > exactNumber) {
+        System.out.println("Too high. You have only " + i + " lives left.");
+      } else if (guess == exactNumber) {
+        System.out.println("Congrats, you won!");
+        break;
+      }
     }
   }
 }
-//    Write a program where the program chooses a number between 1 and 100.
-// The player is then asked to enter a guess. If the player guesses wrong, then the program gives feedback and ask to enter an other guess until the guess is correct.
-//    Make the range customizable (ask for it before starting the guessing).
-//    You can add lives. (optional)
-//        Example
-//    I've the number between 1-100. You have 5 lives.
-//    20
-//    Too high. You have 4 lives left.
-//    10
-//    Too low. You have 3 lives left.
-//    15
-//    Congratulations. You won!
