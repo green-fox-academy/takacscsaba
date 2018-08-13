584d29ff08200dcb63b2fbe1bc4422a9b90faedb
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-13 09:25:07 +0200

    GuessMyNumber progress

--- week-02/day-01/src/GuessMyNumber.java
+++ week-02/day-01/src/GuessMyNumber.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class GuessMyNumber {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please enter the maximum number: ");
+    int input = scanner.nextInt();
+
 
   }
-  public static String
+  public static String myNum(String string, int input) {
+    double exactNumber = Math.random() * input;
+    int lives = 5;
+
+    for (int i = 0; i < 5; i++) {
+      //scanner, input, lives
+    }
+  }
 }
 //    Write a program where the program chooses a number between 1 and 100.
 // The player is then asked to enter a guess. If the player guesses wrong, then the program gives feedback and ask to enter an other guess until the guess is correct.
