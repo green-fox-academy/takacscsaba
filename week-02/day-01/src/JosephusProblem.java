2ac9d46b4bbfda3085cb448ed41611be7bd471af
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-13 17:22:16 +0200

    JosephusProblem init - created function

---
+++ week-02/day-01/src/JosephusProblem.java
@@ -0,0 +1,19 @@
+public class JosephusProblem {
+  public static void main(String[] args) {
+
+  }
+//  public static int winner(int gamersNumber) {
+//      }
+}
+// Write a function to solve Josephus Problem.
+// The program should ask for a number, this number represents how many people are in the "game".
+// The return value should be the number of the "winning" seat.
+//
+//    Examples
+//    number of people	number of the winning seat
+//    1	                1
+//    2	                1
+//    3	                3
+//    7	                7
+//    12	              9
+//    41	              19
