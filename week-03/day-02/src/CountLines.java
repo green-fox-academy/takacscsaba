ed763c1c248a3e62179033663ef6117a517a6a1c
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-14 11:54:04 +0200

    PrintEachLine - corrected; CountLines init

---
+++ week-03/day-02/src/CountLines.java
@@ -0,0 +1,10 @@
+public class CountLines {
+  public static void main(String[] args) {
+
+  }
+  public static String filename;
+}
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
--- week-03/day-02/src/PrintEachLine.java
+++ week-03/day-02/src/PrintEachLine.java
@@ -7,8 +7,9 @@ import java.util.List;
 public class PrintEachLine {
   public static void main(String[] args) {
     try {
-      Path filePath = Paths.get("my-file.txt");
+      Path filePath = Paths.get("src/my-file.txt");
       List<String> lines = Files.readAllLines(filePath);
+      System.out.println(lines.get(0));
     } catch (IOException e) {
       System.out.println("Unable to read file: my-file.txt");
     }
