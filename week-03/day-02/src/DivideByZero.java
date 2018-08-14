3aeb50d19ad7f51c946378bf766d7a2aea47a3eb
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-14 11:36:54 +0200

    DivideByZero finished

--- week-03/day-02/src/DivideByZero.java
+++ week-03/day-02/src/DivideByZero.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class DivideByZero {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Please enter a number: ");
+    int input = scanner.nextInt();
+
+    dividesTen(input);
+  }
+
+  public static int dividesTen(int number) {
+    try {
+      System.out.println(number = 10 / number);
+    } catch (ArithmeticException e) {
+      System.out.println("fail");
+    }
+    return number;
+  }
 }
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print "fail" if the parameter is 0
