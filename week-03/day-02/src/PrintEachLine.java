614cdeb08f04fff81aa966d45aa18651cc645590
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-14 11:55:56 +0200

    PrintEachLine - corrected

--- week-03/day-02/src/PrintEachLine.java
+++ week-03/day-02/src/PrintEachLine.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class PrintEachLine {
   public static void main(String[] args) {
     try {
-      Path filePath = Paths.get("src/my-file.txt");
+      Path filePath = Paths.get("my-file.txt");
       List<String> lines = Files.readAllLines(filePath);
       System.out.println(lines.get(0));
     } catch (IOException e) {
