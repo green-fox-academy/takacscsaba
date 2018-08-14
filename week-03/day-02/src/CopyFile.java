5c053434e789605587ca2e5a593dc92dfc3e4c0c
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-14 14:26:22 +0200

    Logs init

--- week-03/day-02/src/CopyFile.java
+++ week-03/day-02/src/CopyFile.java
@@ -24,6 +24,3 @@ public class CopyFile {
     return true;
   }
 }
-// Write a function that copies a file to an other
-// It should take the filenames as parameters
-// It should return a boolean that shows if the copy was successful
