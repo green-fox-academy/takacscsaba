a1e64791d0aa155546a941f9117b61b8472a830d
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-14 15:50:29 +0200

    Logs - uniqueIP function finished

--- week-03/day-02/src/Logs.java
+++ week-03/day-02/src/Logs.java
@@ -2,33 +2,33 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Logs {
   public static void main(String[] args) {
     Path path = Paths.get("logs.txt");
-    System.out.println(IP(path));
+    System.out.println(uniqueIP(path));
 
   }
 
-  public static List<String> IP(Path path) {
+  public static List<String> uniqueIP(Path path) {
     List<String> uniqueIPList = new ArrayList<>();
     try {
       List<String> lines = Files.readAllLines(path);
       for (int i = 0; i < lines.size(); i++) {
 //        IPList.add((String) lines.get(i).subSequence(27, 38));
-        String[] stringList = lines.get(i).split("   ");
-        
-        for (int j = 0; j < IPs.length; j++) {
-          if (!uniqueIPList.contains(IPs))
+        String IPList = lines.get(i).split("   ")[1];
+
+          if (!uniqueIPList.contains(IPList)) {
+            uniqueIPList.add(IPList);
         }
       }
     } catch (Exception e) {
     }
-    return IPList;
+    return uniqueIPList;
   }
 }
 // Read all data from 'log.txt'.
 // Each line represents a log message from a web server
-// Write a function that returns an array with the unique IP adresses.
 // Write a function that returns the GET / POST request ratio.
