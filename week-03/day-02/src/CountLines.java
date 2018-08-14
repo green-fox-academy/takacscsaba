6888abb63727ecde26fd4d615432ee204682c3b6
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-14 12:06:49 +0200

    CountLines finished

--- week-03/day-02/src/CountLines.java
+++ week-03/day-02/src/CountLines.java
@@ -1,8 +1,24 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class CountLines {
   public static void main(String[] args) {
-
+    String filename = "my-file.txt";
+    System.out.println(lineNum(filename));
   }
-  public static String filename;
+
+  public static int lineNum(String string) {
+    try {
+      Path filePath = Paths.get(string);
+      List<String> lines = Files.readAllLines(filePath);
+      int number = lines.size();
+      return number;
+    } catch (Exception e) {
+      return 0;
+    }
+  }
 }
 // Write a function that takes a filename as string,
 // then returns the number of lines the file contains.
