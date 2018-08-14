eee182bdc56770814cf471e63f560c32a5a12d2d
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-14 13:19:22 +0200

    SymmetricMatrix finished

--- week-03/day-02/src/SymmetricMatrix.java
+++ week-03/day-02/src/SymmetricMatrix.java
@@ -4,26 +4,47 @@ import java.util.Arrays;
 public class SymmetricMatrix {
 
   public static void main(String[] args) {
-    // Create a function named `isSymmetric` that takes an n×n integer matrix (list of lists) as parameter
-    // and returns true, if is that matrix is symmetric (diagonally from top-left to bottom-right)
-    // or false if it is not
-    //
-    // example for symmetric matrix:
-    // 1 0 1
-    // 0 2 2
-    // 1 2 5
-    //
-    // example for not symmetric matrix
-    // 7 7 7
-    // 6 5 7
-    // 1 2 1
-
     ArrayList<ArrayList<Integer>> symmetricMatrix = new ArrayList<ArrayList<Integer>>();
+    symmetricMatrix.add(new ArrayList<Integer>(Arrays.asList(1, 0, 1)));
+    symmetricMatrix.add(new ArrayList<Integer>(Arrays.asList(0, 2, 2)));
+    symmetricMatrix.add(new ArrayList<Integer>(Arrays.asList(1, 2, 5)));
 
-    symmetricMatrix.add(new ArrayList<Integer> (Arrays.asList(1, 0, 1)));
-    symmetricMatrix.add(new ArrayList<Integer> (Arrays.asList(0, 2, 2)));
-    symmetricMatrix.add(new ArrayList<Integer> (Arrays.asList(1, 2, 5)));
+    System.out.println(isSymmetric(symmetricMatrix));
+  }
 
-    System.out.println(isSymmetric(symmetricMatrix)); // should print out true
+  public static boolean isSymmetric(ArrayList<ArrayList<Integer>> m) {
+
+    for (int row = 0; row < m.size(); row++) {
+      if (m.size() != m.get(row).size()) {
+        return false;
+      }
+      for (int column = 0; column < m.size(); column++) {
+        if (m.get(row).get(column) != m.get(column).get(row)) {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 }
+//   Create a function named `isSymmetric` that takes an n×n integer matrix (list of lists) as parameter
+//   and returns true, if is that matrix is symmetric (diagonally from top-left to bottom-right)
+//   or false if it is not
+//
+//   example for symmetric matrix:
+//   1 0 1
+//   0 2 2
+//   1 2 5
+//
+//   example for not symmetric matrix
+//   7 7 7
+//   6 5 7
+//   1 2 1
+//
+//  ArrayList<ArrayList<Integer>> symmetricMatrix = new ArrayList<ArrayList<Integer>>();
+//
+//    symmetricMatrix.add(new ArrayList<Integer> (Arrays.asList(1, 0, 1)));
+//    symmetricMatrix.add(new ArrayList<Integer> (Arrays.asList(0, 2, 2)));
+//    symmetricMatrix.add(new ArrayList<Integer> (Arrays.asList(1, 2, 5)));
+//
+//    System.out.println(isSymmetric(symmetricMatrix)); // should print out true
