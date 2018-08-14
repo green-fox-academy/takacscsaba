ad13e957f1c926ca0afdbeb6ba360424d9771cee
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-14 11:40:25 +0200

    PrintEachLine init

--- week-03/day-02/src/DivideByZero.java
+++ week-03/day-02/src/DivideByZero.java
@@ -18,7 +18,3 @@ public class DivideByZero {
     return number;
   }
 }
-// Create a function that takes a number
-// divides ten with it,
-// and prints the result.
-// It should print "fail" if the parameter is 0
---
+++ week-03/day-02/src/PrintEachLine.java
@@ -0,0 +1,15 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+
+public class PrintEachLine {
+  public static void main(String[] args) {
+
+
+  }
+  
+}
+// Write a program that opens a file called "my-file.txt", then prints
+// each of the lines form the file.
+// If the program is unable to read the file (for example it does not exists),
+// then it should print an error message like: "Unable to read file: my-file.txt"
