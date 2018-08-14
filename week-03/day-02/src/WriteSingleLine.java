e0aa00fae0a15792e7e827b1bfd6817f3df05d4e
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-14 12:38:27 +0200

    WriteSingleLine finished

--- week-03/day-02/src/WriteSingleLine.java
+++ week-03/day-02/src/WriteSingleLine.java
@@ -1,9 +1,28 @@
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WriteSingleLine {
   public static void main(String[] args) {
+    String filename = "my-file.txt";
+    writeOpen(filename);
+  }
+  public static void writeOpen(String string) {
+    try {
+      List<String> content = new ArrayList();
+      content.add("Takács Csaba");
+      Path filePath = Paths.get(string);
+      Files.write(filePath, content);
+
+      List<String> lines = Files.readAllLines(filePath);
+      for (int i = 0; i <= lines.size() ; i++) {
+        System.out.println(lines.get(i));
+      }
+    } catch (Exception e) {
+      System.out.println("Unable to write file: my-file.txt");
+    }
   }
 }
 // Open a file called "my-file.txt"
