92166992c9ad9cac26ecf5f007b42b3e43e19d2d
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-15 16:00:51 +0200

    FleetOfThings finished

--- week-03/day-03/FleetOfThings/src/Main.java
+++ week-03/day-03/FleetOfThings/src/Main.java
@@ -1,14 +1,20 @@
 public class Main {
 
-    public static void main(String[] args) {
-        Fleet fleet = new Fleet();
-        // Create a fleet of things to have this output:
-        // 1. [ ] Get milk
-        // 2. [ ] Remove the obstacles
-        // 3. [x] Stand up
-        // 4. [x] Eat lunch
+  public static void main(String[] args) {
+    Fleet fleet = new Fleet();
+    Thing milk = new Thing("Get milk");
+    Thing obstacles = new Thing("Remove the obstacles");
+    Thing standUp = new Thing("standUp");
+    Thing eat = new Thing("Eat lunch");
 
-        System.out.println(fleet);
-    }
+    standUp.complete();
+    eat.complete();
+    fleet.add(milk);
+    fleet.add(obstacles);
+    fleet.add(standUp);
+    fleet.add(eat);
+
+    System.out.println(fleet);
+  }
 }
 
