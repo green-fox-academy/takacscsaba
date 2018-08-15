cf9e8c9441bc313c1b6501a1f28123f3a84e05ac
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-15 12:59:17 +0200

    PostIt finished

---
+++ week-03/day-03/PostIt/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+  public static void main(String[] args) {
+    PostIt postIt1 = new PostIt();
+    postIt1.backgroundColor = "Orange";
+    postIt1.text = "Idea 1";
+    postIt1.textColor = "Blue";
+
+    PostIt postIt2 = new PostIt();
+    postIt1.backgroundColor = "Pink";
+    postIt1.text = "Awesome";
+    postIt1.textColor = "Black";
+
+    PostIt postIt3 = new PostIt();
+    postIt1.backgroundColor = "Yellow";
+    postIt1.text = "Superb!";
+    postIt1.textColor = "Green";
+  }
+}
