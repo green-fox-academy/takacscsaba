b8020c1872cfb41900bc026c0c88bb7dc2a14b42
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-15 11:51:46 +0200

    Doubled finished - was overcomplicated, now works perfectly

--- week-03/day-02/src/Doubled.java
+++ week-03/day-02/src/Doubled.java
@@ -12,25 +12,17 @@ public class Doubled {
   }
 
   public static void everySecond(Path path) {
-    List<String> string = new ArrayList<>();
     List<Character> chars = new ArrayList<>();
     try {
       List<String> lines = Files.readAllLines(path);
       for (int i = 0; i < lines.size(); i++) {
-        string.add(lines.get(i) + "\n");
-      }
-      for (int i = 0; i < string.size(); i++) {
+        String oneline = new String();
         for (int j = 0; j < lines.get(i).length(); j = j + 2) {
-          chars.add(string.get(i).charAt(j));
+          oneline += lines.get(i).charAt(j);
         }
+        System.out.println(oneline);
       }
     } catch (Exception e) {
     }
-    StringBuilder builder = new StringBuilder(chars.size());
-    for (Character ch : chars) {
-      builder.append(ch);
-    }
-    System.out.println(builder.toString());
   }
 }
-// Create a method that decrypts the duplicated-chars.txt
