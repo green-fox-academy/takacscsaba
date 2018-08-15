e21dd968a27961f9e1de9b72c227fc626b27e747
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-15 16:56:25 +0200

    Counter modification

--- week-03/day-03/Counter/src/Counter.java
+++ week-03/day-03/Counter/src/Counter.java
@@ -12,7 +12,7 @@ public class Counter {
 
 
   public int add(int number) {
-    field += field + number;
+    field += number;
     return field;
   }
 
@@ -27,7 +27,7 @@ public class Counter {
   }
 
   public int reset() {
-    this.field = 0;
+    field = 0;
     return field;
   }
 }
@@ -38,7 +38,3 @@ public class Counter {
 //    or we can add() without parameters just increasing the counter's value by one
 //    and we can get() the current value
 //    also we can reset() the value to the initial value
-//    Check if everything is working fine with the proper test
-//    Download CounterTest.java and place it next to your solution
-//    If something's red in it, ALT+Enter Add JUnit5 to classpath (5 aka JUnit FIVE not 4 or 3)
-//    Then run the tests with the green play button before the lines (run all of them before the classname)
