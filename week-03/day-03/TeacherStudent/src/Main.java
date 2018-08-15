80be4fc816c31dd6c0c75640ce77b7c976be752d
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-15 16:24:29 +0200

    TeacherStudent init

---
+++ week-03/day-03/TeacherStudent/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+}
+//  Create Student and Teacher classes
+//    Student
+//      learn()
+//      question(teacher) -> calls the teachers answer method
+//    Teacher
+//      teach(student) -> calls the students learn method
+//      answer()
---
+++ week-03/day-03/TeacherStudent/src/Teacher.java
@@ -0,0 +1,16 @@
+public class Teacher {
+
+  public void answer() {
+  }
+
+  public void teach(Student student) {
+
+  }
+}
+//  Create Student and Teacher classes
+//    Student
+//      learn()
+//      question(teacher) -> calls the teachers answer method
+//    Teacher
+//      teach(student) -> calls the students learn method
+//      answer()
