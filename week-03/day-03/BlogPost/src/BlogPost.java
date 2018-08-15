00bd0ee2b6e04d412e876d58f5ad750927ebc947
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-15 13:23:28 +0200

    BlogPost variation

--- week-03/day-03/BlogPost/src/BlogPost.java
+++ week-03/day-03/BlogPost/src/BlogPost.java
@@ -1,6 +1,20 @@
 public class BlogPost {
   String authorName;
+  String publicationDate;
   String title;
   String text;
-  String publicationDate;
+
+  public BlogPost() {
+    this.authorName = "";
+    this.publicationDate = "";
+    this.title = "";
+    this.text = "";
+  }
+
+  public BlogPost(String authorName, String publicationDate, String title, String text) {
+    this.authorName = authorName;
+    this.publicationDate = publicationDate;
+    this.title = title;
+    this.text = text;
+  }
 }
