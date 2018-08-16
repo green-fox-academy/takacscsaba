71b8168ed9324ffb0261eddb1670869629eeab69
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-16 13:49:46 +0200

    String2 finished

--- week-03/day-04/src/String2.java
+++ week-03/day-04/src/String2.java
@@ -1,19 +1,19 @@
-public class String1 {
+public class String2 {
   public static void main(String[] args) {
     String text = "xenoxoxtaxy";
     System.out.println(x2y(text));
   }
 
+
   public static String x2y(String s) {
     int length = s.length();
+
     if (length == 0) {
       return s;
     } else if (s.startsWith("x")) {
-      return "y" + x2y(s.substring(1));
+      return x2y(s.substring(1));
     } else {
       return s.charAt(0) + x2y(s.substring(1));
     }
   }
 }
-// Given a string, compute recursively (no loops) a new string where all the
-// lowercase 'x' chars have been changed to 'y' chars.
