a07f3ab29479957219780c035590aa4d5066b568
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-16 11:40:47 +0200

    Power finished

--- week-03/day-04/src/Power.java
+++ week-03/day-04/src/Power.java
@@ -13,5 +13,3 @@ public class Power {
     }
   }
 }
-// Given base and n that are both 1 or more, compute recursively (no loops)
-// the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
