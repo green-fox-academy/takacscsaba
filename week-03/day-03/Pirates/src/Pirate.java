3d43f36c2bc379105e28aa6a1d6ed0ab32c4ee9c
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-19 20:55:28 +0200

    Pirate Class finished

--- week-03/day-03/Pirates/src/Pirate.java
+++ week-03/day-03/Pirates/src/Pirate.java
@@ -1,5 +1,48 @@
 public class Pirate {
-  public static void main(String[] args) {
-    
+  int intoxication = 0;
+  boolean passedOut = false;
+  boolean dead = false;
+
+  public void drinkSumRum() {
+    if (dead) {
+      System.out.println("He's dead");
+    } else {
+      intoxication++;
+    }
+  }
+
+  public void howsItGoingMate() {
+    if (dead) {
+      System.out.println("He's dead");
+    } else if (intoxication < 5) {
+      System.out.println("Pour me anudder!");
+    } else {
+      System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+      passOut();
+    }
+  }
+
+  public void die() {
+    dead = true;
+  }
+
+  public void passOut () {
+    passedOut = true;
+  }
+
+  public void brawl(Pirate x) {
+    int chanceToDie = (int) (Math.random() * 3) + 1;
+    switch (chanceToDie) {
+      case 1:
+        this.die();
+        break;
+      case 2:
+        x.die();
+        break;
+      case 3:
+        this.passOut();
+        x.passOut();
+        break;
+    }
   }
 }
