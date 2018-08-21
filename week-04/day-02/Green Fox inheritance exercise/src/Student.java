37548848b8693064398f8c38376328608a642dde
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-21 11:12:01 +0200

    Student Class created and filled

---
+++ week-04/day-02/Green Fox inheritance exercise/src/Student.java
@@ -0,0 +1,40 @@
+public class Student extends Person {
+  String previousOrganization;
+  int skippedDays;
+
+  public void getGoal() {
+    System.out.println("Be a junior software developer.");
+  }
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + " from " + previousOrganization + " who skipped " + skippedDays + " days from the course already.");
+  }
+
+  public void skipDays(int numberOfDays) {
+    skippedDays++;
+  }
+
+  public Student(String name, int age, String gender, String previousOrganization) {
+    skippedDays = 0;
+  }
+
+  public Student() {
+    name = "Jane Doe";
+    age = 30;
+    gender = "female";
+    previousOrganization = "The School of Life";
+    skippedDays = 0;
+  }
+}
+//    additional fields:
+//    previousOrganization: the name of the student’s previous company / school
+//    skippedDays: the number of days skipped from the course
+//
+//    methods:
+//    getGoal(): prints out "Be a junior software developer."
+//    introduce(): "Hi, I'm name, a age year old gender from previousOrganization who skipped skippedDays days from the course already."
+//    skipDays(numberOfDays): increases skippedDays by numberOfDays
+//
+//    The Student class has the following constructors:
+//    Student(name, age, gender, previousOrganization): beside the given parameters, it sets skippedDays to 0
+//    Student(): sets name to Jane Doe, age to 30, gender to female, previousOrganization to The School of Life, skippedDays to 0
