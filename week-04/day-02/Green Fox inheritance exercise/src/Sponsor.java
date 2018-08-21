eb99c9085cedfa3524d4ec7c94b0277652212822
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-21 11:21:14 +0200

    Sponsor Class created and filled

---
+++ week-04/day-02/Green Fox inheritance exercise/src/Sponsor.java
@@ -0,0 +1,28 @@
+public class Sponsor extends Person {
+  String company;
+  int hiredStudents;
+
+  public void introduce() {
+    System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + " who represents " + company + " and hired " + hiredStudents + " students so far.");
+  }
+
+  public void hire() {
+    hiredStudents++;
+  }
+
+  public void getGoal() {
+    System.out.println("Hire brilliant junior software developers.");
+  }
+
+  public Sponsor(String name, int age, String gender, String company) {
+    hiredStudents = 0;
+  }
+
+  public Sponsor() {
+    name = "Jane Doe";
+    age = 30;
+    gender = "female";
+    company = "Google";
+    hiredStudents = 0;
+  }
+}
