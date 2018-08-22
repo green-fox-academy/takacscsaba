7949756d53a8d6c311c7cbe3e9555be1d313580c
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-22 15:30:35 +0200

    ITSI Classes created

--- week-04/day-03/InstrumentsToStringedInstruments/src/Instrument.java
+++ week-04/day-03/InstrumentsToStringedInstruments/src/Instrument.java
@@ -1,2 +1,6 @@
 public abstract class Instrument {
+  protected String name;
+
+  public void play() {
+  }
 }
---
+++ week-04/day-03/InstrumentsToStringedInstruments/src/StringedInstrument.java
@@ -0,0 +1,10 @@
+public abstract class StringedInstrument extends Instrument {
+  int numberOfStrings;
+
+  public void sound() {
+  }
+
+  public void play() {
+    sound();
+  }
+}
