f0328089baed7e04efcadd9150b82425c9d8ac47
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-23 15:17:46 +0200

    SumTest finished

---
+++ week-04/day-04/Anagram/src/Anagram.java
@@ -0,0 +1,7 @@
+public class Anagram {
+  
+
+}
+
+//  Write a function, that takes two strings and returns a boolean value based on if the two strings are Anagramms or not.
+//    Create a test for that.
--- week-04/day-04/Sum/test/SumTest.java
+++ week-04/day-04/Sum/test/SumTest.java
@@ -5,18 +5,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SumTest {
-  int expectedSum = 6;
   Sum sum = new Sum();
 
   @Test
   public void shouldReturnSix() {
     List<Integer> listSixTest = new ArrayList<>();
     listSixTest.add(6);
+    int expectedSum = 6;
     Assert.assertEquals(expectedSum, sum.getSum(listSixTest));
   }
 
   @Test
-  public void emptyList(){
-    
+  public void emptyListTest() {
+    List<Integer> emptyListTest = new ArrayList<>();
+    int expectedResult = 0;
+    int shouldBeZero = sum.getSum(emptyListTest);
+    Assert.assertEquals(expectedResult, shouldBeZero);
+  }
+
+  @Test
+  public void oneElementTest() {
+    List<Integer> oneElementList = new ArrayList<>();
+    oneElementList.add(311);
+    int expectedResult = 311;
+    int oneElement = sum.getSum(oneElementList);
+    Assert.assertEquals(expectedResult, oneElement);
+  }
+
+  @Test
+  public void multipleElementTest() {
+    List<Integer> multipleElementList = new ArrayList<>();
+    multipleElementList.add(311);
+    multipleElementList.add(112);
+    multipleElementList.add(23);
+    int expectedResult = 311 + 112 + 23;
+    int sumResult = sum.getSum(multipleElementList);
+    Assert.assertEquals(expectedResult, sumResult);
+  }
+
+  @Test
+  public void nullListTest() {
+    List<Integer> nullList = new ArrayList<>();
+    nullList.add(0);
+    int expectedResult = 0;
+    Assert.assertEquals(expectedResult, sum.getSum(nullList));
   }
 }
