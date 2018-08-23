26adc208b575d076d2500b03a6ff3a363993ee15
Csaba Takacs <takacs.csaba@hotmail.com>
2018-08-23 15:45:06 +0200

    CountLetters init - created counter method

---
+++ week-04/day-04/CountLetters/src/CountLetters.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountLetters {
+  public Map<Character, Integer> counter(String string) {
+    Map<Character, Integer> dictionary = new HashMap<>();
+
+  }
+}
+
+//  Write a function, that takes a string as an argument and returns a dictionary with all letters in the string as keys, and numbers as values that shows how many occurrences there are.
