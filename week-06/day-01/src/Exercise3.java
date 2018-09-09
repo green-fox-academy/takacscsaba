c1346de9e6a6901db5c713fb91bf165a789d8702
Csaba Takacs <takacs.csaba@hotmail.com>
2018-09-09 13:24:27 +0200

    Exercise3 finished

--- week-06/day-01/src/Exercise3.java
+++ week-06/day-01/src/Exercise3.java
@@ -7,6 +7,11 @@ public class Exercise3 {
 //    Write a Stream Expression to get the squared value of the positive numbers from the following array:
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
 
+    numbers.stream()
+           .filter(x -> x % 2 ==0)
+           .mapToInt(x -> x*x)
+           .forEach(System.out::println);
+
     System.out.println(sqrValueMethod(numbers));
   }
 
