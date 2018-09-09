ed10e545bd99fd445b5142951a451aa042472bc2
Csaba Takacs <takacs.csaba@hotmail.com>
2018-09-09 12:50:28 +0200

    Exercise2 method finished

---
+++ week-06/day-01/src/Exercise2.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise2 {
+  public static void main(String[] args) {
+//    Write a Stream Expression to get the average value of the odd numbers from the following array:
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+
+    System.out.println(avrValueMethod(numbers));
+  }
+
+  public static double avrValueMethod(ArrayList<Integer> numbers) {
+    double avrValue = 0;
+    for (int i = 0; i < numbers.size(); i++) {
+      avrValue += numbers.get(i);
+    }
+    double avr = avrValue / numbers.size();
+    return avr;
+  }
+}
