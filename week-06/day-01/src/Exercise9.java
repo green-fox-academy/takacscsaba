1d04f9eb567efb4ea21c12506a392b7c1efe08e7
Csaba Takacs <takacs.csaba@hotmail.com>
2018-09-09 18:02:42 +0200

    Exercise9 finished

--- week-06/day-01/src/Exercise9.java
+++ week-06/day-01/src/Exercise9.java
@@ -1,17 +1,26 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Exercise9 {
   public static void main(String[] args) {
 //    Write a Stream Expression to convert a char array to a string!
-    ArrayList<Character> charArray = new ArrayList<>(Arrays.asList('s', 'o', 'n', '.', 'g', 'o', 'h', 'a', 'n', 's', 's', 'j', '2'));
+    ArrayList<Character> charArray = new ArrayList<>(Arrays.asList('s', 'o', 'n', '.', 'g', 'o', 'h', 'a', 'n', 'S', 'S', 'J', '2'));
 
-    
+    System.out.println(
+        charArray.stream()
+            .map(String::valueOf)
+            .collect(Collectors.joining()));
 
     System.out.println(charToStringMethod(charArray));
   }
 
   public static String charToStringMethod(ArrayList<Character> charArray) {
-    return charArray.toString().toString().replaceAll(", ", "").replace("[", "").replace("]", "");
+    StringBuilder stringBuilder = new StringBuilder();
+    for (Character ch : charArray) {
+      stringBuilder.append(ch);
+    }
+    return stringBuilder.toString();
+//    return charArray.toString().replaceAll(", ", "").replace("[", "").replace("]", "");
   }
 }
