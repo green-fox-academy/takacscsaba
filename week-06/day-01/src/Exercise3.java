b52db861bb739d72aa69b89c08998431475c79e5
Csaba Takacs <takacs.csaba@hotmail.com>
2018-09-09 13:22:54 +0200

    Exercise3 method finished

---
+++ week-06/day-01/src/Exercise3.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise3 {
+  public static void main(String[] args) {
+//    Write a Stream Expression to get the squared value of the positive numbers from the following array:
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+
+    System.out.println(sqrValueMethod(numbers));
+  }
+
+  public static List<Integer> sqrValueMethod(ArrayList<Integer> numbers) {
+    List<Integer> sqrValue = new ArrayList<>();
+    for (int i = 0; i < numbers.size(); i++) {
+      if (numbers.get(i) % 2 ==0) {
+        sqrValue.add(numbers.get(i) * numbers.get(i));
+      }
+    }
+    return sqrValue;
+  }
+}
