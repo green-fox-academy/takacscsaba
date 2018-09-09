b9810b1780dd02fdbd798886092b3cc96af7ed3c
Csaba Takacs <takacs.csaba@hotmail.com>
2018-09-09 18:18:54 +0200

    Created six foxes

--- week-06/day-01/src/main/java/foxes/Main.java
+++ week-06/day-01/src/main/java/foxes/Main.java
@@ -1,7 +1,16 @@
 package main.java.foxes;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-    
+    List<Fox> foxList = new ArrayList<>();
+    foxList.add(new Fox("RokaUr", "foxyfox", "green"));
+    foxList.add(new Fox("RokaKoma", "foxer", "red"));
+    foxList.add(new Fox("RavaszRoka", "focks", "black"));
+    foxList.add(new Fox("RoKalapos", "foxyfox", "green"));
+    foxList.add(new Fox("RoKalander", "forox", "white"));
+    foxList.add(new Fox("RoKapuska", "fogox", "green"));
+
   }
 }
