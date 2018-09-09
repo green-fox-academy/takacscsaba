9cbf671d56308a32fcfa487f04d30c84430753bf
Csaba Takacs <takacs.csaba@hotmail.com>
2018-09-09 18:11:33 +0200

    Exercise10 Fox and Main Class created

---
+++ week-06/day-01/src/main/java/foxes/Fox.java
@@ -0,0 +1,14 @@
+package main.java.foxes;
+//    Create a Fox class with 3 properties(name, type, color) Fill a list with at least 5 foxes, it's up to you how you name/create them! Write a Stream Expression to find the foxes with green color! Write a Stream Expression to find the foxes with green color and pallida type!
+
+public class Fox {
+  String name;
+  String type;
+  String color;
+
+  public Fox(String name, String type, String color) {
+    this.name = name;
+    this.type = type;
+    this.color = color;
+  }
+}
---
+++ week-06/day-01/src/main/java/foxes/Main.java
@@ -0,0 +1,7 @@
+package main.java.foxes;
+
+public class Main {
+  public static void main(String[] args) {
+    
+  }
+}
