eaf328cc7c14a3b3fb46b09fe41d641ce73dd49a
Csaba Takacs <takacs.csaba@hotmail.com>
2018-09-18 17:24:20 +0200

    output in table yeey

--- week-07/day-02-2/src/main/java/com/greenfoxacademy/bankofsimba2/controller/MainController.java
+++ week-07/day-02-2/src/main/java/com/greenfoxacademy/bankofsimba2/controller/MainController.java
@@ -1,8 +1,6 @@
 package com.greenfoxacademy.bankofsimba2.controller;
 
 import com.greenfoxacademy.bankofsimba2.models.BankAccount;
-import com.greenfoxacademy.bankofsimba2.services.BankServiceImp;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +10,14 @@ import java.util.List;
 @Controller
 public class MainController {
 
+  private BankAccount bankAccountCreater() {
+    BankAccount bankAccount = new BankAccount("Simba", 2000.0, "lion");
+    return bankAccount;
+  }
+
   @GetMapping("")
   public String showingAccount(Model model) {
-    model.addAttribute();
+    model.addAttribute("bankAccount", bankAccountCreater());
     return "show";
   }
 
