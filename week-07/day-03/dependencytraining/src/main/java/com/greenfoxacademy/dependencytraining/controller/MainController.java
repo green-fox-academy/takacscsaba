ea3d035bd8eceebe82c5c26f75540e35e04698e6
Csaba Takacs <takacs.csaba@hotmail.com>
2018-09-19 17:25:35 +0200

    third task finished

--- week-07/day-03/dependencytraining/src/main/java/com/greenfoxacademy/dependencytraining/controller/MainController.java
+++ week-07/day-03/dependencytraining/src/main/java/com/greenfoxacademy/dependencytraining/controller/MainController.java
@@ -1,14 +1,34 @@
 package com.greenfoxacademy.dependencytraining.controller;
 
+import com.greenfoxacademy.dependencytraining.services.UtilityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+  @Autowired
+  UtilityService utilityService;
+
+  public MainController(UtilityService utilityService) {
+    this.utilityService = utilityService;
+  }
 
   @GetMapping("")
-  public String mapper(Model model) {
+  public String index() {
     return "index";
+  }
+
+  @GetMapping("/useful")
+  public String useful(Model model) {
+    model.addAttribute("colors", utilityService.getColors());
+    return "useful";
+  }
+
+  @GetMapping("/useful/colored")
+  public String usefulColors(Model model) {
+    model.addAttribute("colors", utilityService.randomColor());
+    return "colored";
   }
 }
---
+++ week-07/day-03/dependencytraining/src/main/java/com/greenfoxacademy/dependencytraining/services/UtilityService.java
@@ -0,0 +1,47 @@
+package com.greenfoxacademy.dependencytraining.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+@Service
+public class UtilityService {
+  ArrayList<String> colors;
+  Random random;
+
+  public UtilityService() {
+    colors = new ArrayList<>();
+    colors.add("red");
+    colors.add("blue");
+    colors.add("lime");
+    colors.add("orange");
+    colors.add("magenta");
+    random = new Random();
+  }
+
+  public UtilityService(ArrayList<String> colors, Random random) {
+    this.colors = colors;
+    this.random = random;
+  }
+
+  public String randomColor() {
+    return colors.get(random.nextInt(colors.size()));
+  }
+
+  public ArrayList<String> getColors() {
+    return colors;
+  }
+
+  public void setColors(ArrayList<String> colors) {
+    this.colors = colors;
+  }
+
+  public Random getRandom() {
+    return random;
+  }
+
+  public void setRandom(Random random) {
+    this.random = random;
+  }
+}
