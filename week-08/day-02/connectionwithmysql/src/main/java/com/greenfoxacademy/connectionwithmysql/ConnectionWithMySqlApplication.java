3457b2b581477abba3aa169aa66f7b3a5259864d
Csaba Takacs <takacs.csaba@hotmail.com>
2018-09-25 14:58:00 +0200

    connectionwithmysql project init

---
+++ week-08/day-02/connectionwithmysql/src/main/java/com/greenfoxacademy/connectionwithmysql/ConnectionWithMySqlApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.connectionwithmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionWithMySqlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConnectionWithMySqlApplication.class, args);
+  }
+}
