448d4f902274b2e553e4dad7fe59da758dde96e4
Csaba Takacs <takacs.csaba@hotmail.com>
2018-09-26 10:11:03 +0200

    creating new tasks work

--- week-08/day-02/connectionwithmysql/src/main/java/com/greenfoxacademy/connectionwithmysql/controllers/TodoController.java
+++ week-08/day-02/connectionwithmysql/src/main/java/com/greenfoxacademy/connectionwithmysql/controllers/TodoController.java
@@ -1,12 +1,11 @@
 package com.greenfoxacademy.connectionwithmysql.controllers;
 
+import com.greenfoxacademy.connectionwithmysql.models.Todo;
 import com.greenfoxacademy.connectionwithmysql.repositories.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/todo")
@@ -27,5 +26,17 @@ public class TodoController {
     }
     model.addAttribute("todoList", todoRepository.findAll());
     return "todoslist";
+  }
+
+  @GetMapping("/add")
+  public String addTaskPage(Model model) {
+    model.addAttribute("todo", new Todo());
+    return "addtodo";
+  }
+
+  @PostMapping("/add")
+  public String addTask(@ModelAttribute Todo todo) {
+    todoRepository.save(todo);
+    return "redirect:/todo/list";
   }
 }
