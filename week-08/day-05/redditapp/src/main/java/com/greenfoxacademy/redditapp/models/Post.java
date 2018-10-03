fa9a604359e56acf2d051e141efb5d139e08cf37
takacscsaba <takacs.csaba@hotmail.com>
2018-10-03 16:53:24 +0200

    Post model filled

--- week-08/day-05/redditapp/src/main/java/com/greenfoxacademy/redditapp/models/Post.java
+++ week-08/day-05/redditapp/src/main/java/com/greenfoxacademy/redditapp/models/Post.java
@@ -1,7 +1,57 @@
 package com.greenfoxacademy.redditapp.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Post {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
+  String title;
+  String url;
+  Integer count;
+
+  public Post() {
+  }
+
+  public Post(String title, String url) {
+    this.title = title;
+    this.url = url;
+    this.count = 0;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public Integer getCount() {
+    return count;
+  }
+
+  public void setCount(Integer count) {
+    this.count = count;
+  }
 }
